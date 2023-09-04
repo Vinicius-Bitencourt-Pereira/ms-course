da0e1743d08d36cd7e49cd7121d93d3fe87e0554
Vinicius-Bitencourt-Pereira <viniciusbitencourtpereira@gmail.com>
2023-09-04 19:33:35 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/br/com/vinicius/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/br/com/vinicius/hrpayroll/controllers/PaymentController.java
@@ -2,6 +2,7 @@ package br.com.vinicius.hrpayroll.controllers;
 
 import br.com.vinicius.hrpayroll.entities.Payment;
 import br.com.vinicius.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,15 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(path = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }

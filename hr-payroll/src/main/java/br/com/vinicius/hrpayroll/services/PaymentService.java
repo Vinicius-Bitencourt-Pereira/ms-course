32d0264639e903317a725a532c2a0186ef1474c0
Vinicius-Bitencourt-Pereira <viniciusbitencourtpereira@gmail.com>
2023-09-04 17:13:31 -0300

    RestTemplate

--- hr-payroll/src/main/java/br/com/vinicius/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/vinicius/hrpayroll/services/PaymentService.java
@@ -1,11 +1,30 @@
 package br.com.vinicius.hrpayroll.services;
 
 import br.com.vinicius.hrpayroll.entities.Payment;
+import br.com.vinicius.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        if (worker != null) {
+            return new Payment(worker.getName(), worker.getDailyIncome(), days);
+        }
+        return null;
     }
 }
